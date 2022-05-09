d5682cf2e4ec87db1ee2f5dfe47ded7dcbd8d2b0
Elder Guerra <eldernguerra.filho@hotmail.com>
2022-05-09 12:18:07 -0400

    Removing pug, handlebars and html files.. adding ejs templates

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+/* const expressHbs = require('express-handlebars'); */
 
-app.set('view engine', 'pug')
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+/* app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})); */
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found', path: '/404'});
+});
 
-app.listen(3000)
+app.listen(3000);
