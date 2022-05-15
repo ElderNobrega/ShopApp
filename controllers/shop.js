f8a38f565403447b6ce161a0930830472cce61c8
Elder Guerra <eldernguerra.filho@hotmail.com>
2022-05-14 22:01:37 -0400

    Added add to cart, orders page,, Updates on some css

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,22 @@
 const Product = require('../models/product')
 
 
-exports.getProduct = (req, res, next) => {
+exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/product-list', { 
             prods: products, 
             pageTitle: 'All Products', 
+            path: '/products',
+        })
+    })
+}
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId
+    Product.fetchById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product, 
+            pageTitle: product.title, 
             path: '/products',
         })
     })
@@ -25,6 +36,18 @@ exports.getCart = (req, res, next) => {
     res.render('shop/cart', {
         pageTitle: 'Your Cart', 
         path: '/cart',
+    })
+}
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId
+    res.redirect('/cart')
+}
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        pageTitle: 'Your Orders',
+        path: '/orders'
     })
 }
 
--- models/product.js
+++ models/product.js
@@ -14,11 +14,15 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
+        this.id = Math.random().toString()
         getProductsFromFile( products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -29,5 +33,12 @@ module.exports = class Product {
 
     static fetchAll(cb) {
         getProductsFromFile(cb)
+    }
+
+    static fetchById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(product => product.id === id)
+            cb(product)
+        })
     }
 }
