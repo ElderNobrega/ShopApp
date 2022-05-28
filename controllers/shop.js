383f5e31fdf5fea646bdad9ddb7bce6df684d05a
Elder Guerra <eldernguerra.filho@hotmail.com>
2022-05-28 17:49:10 -0400

    implementing cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,7 +41,21 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    Cart.getCart(cart => {
+    req.user.getCart()
+        .then(cart => {
+            return cart.getProducts()
+                .then(products => {
+                    res.render('shop/cart', {
+                        pageTitle: 'Your Cart', 
+                        path: '/cart',
+                        products: products
+                    }) 
+                })
+                .catch(err => {console.log('error: ', err)})
+        })
+        .catch(err => {console.log('error: ', err)})
+
+    /* Cart.getCart(cart => {
         Product.fetchAll(products => {
             const cartProducts = []
             for (product of products) {
@@ -56,15 +70,41 @@ exports.getCart = (req, res, next) => {
                 products: cartProducts
             })
         })
-    })
+    }) */
 }
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId
+    let fetchedCart;
+    req.user.getCart()
+        .then(cart => {
+            fetchedCart = cart
+            return cart.getProducts({ where: {id: prodId}})
+        })
+        .then(products => {
+            let product;
+            if (products.length >0) {
+                product = products[0]
+            }
+            let newQuantity = 1
+            if (product) {
+                //...
+            }
+            return Product.findByPk(prodId)
+                .then(product => {
+                    return fetchedCart.addProduct(product, {through: {quantity: newQuantity}})
+                })
+                .catch(err => {console.log('error: ', err)})
+        })
+        .then(() => {
+            res.redirect('/cart')
+        })
+        .catch(err => console.log('error: ', err))
+    /* const prodId = req.body.productId
     Product.findById(prodId, product => {
         Cart.addProduct(prodId, product.price)
     })
-    res.redirect('/cart')
+    res.redirect('/cart') */
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
