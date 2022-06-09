0e6e39a2b7446d85d6cdb35a1feb3fe7b5e73eee
Elder Guerra <eldernguerra.filho@hotmail.com>
2022-06-08 23:32:32 -0400

    Adding connect-mongodb-session lib

--- app.js
+++ app.js
@@ -5,20 +5,29 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const loginRoutes = require('./routes/auth')
 const errorController = require('./controllers/error')
+const User = require('./models/user')
 const dbConectionString = require('./util/database').dbConectionString
 const mongoose = require('mongoose')
-const User = require('./models/user')
+const session = require('express-session')
+const mongoDBStore = require('connect-mongodb-session')(session)
 
 
 const app = express();
+const store = new mongoDBStore({
+    uri: dbConectionString,
+    collection: 'sessions'
+})
 
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({ 
+        secret: 'my secrete', resave: false, saveUninitialized: false, store: store
+    })
+)
 
 app.use((req, res, next) => {
     User.findById('629fce142321018f9c1327bd')
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').trim().split('=')[1]
+    // const isLoggedIn = req.get('Cookie').trim().split('=')[1]
+    console.log('session: ', req.session.isLoggedIn)
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     })
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true')
+    req.session.isLoggedIn = true
     res.redirect('/')
 }
